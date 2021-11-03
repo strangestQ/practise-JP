4d4b9f903f593155fd63d5ea5efa7337b9275968
strangestQ <jimmyoliven@yahoo.co.jp>
2021-11-03 20:03:21 +0900

    練習

--- homework.c
+++ homework.c
@@ -928,3 +928,346 @@
 //	}
 //	return 0;
 //}
+//新明解C言語入門演習5-1
+//int main()
+//{
+//	int a[5];
+//	for (int i = 0; i < 5; i++)
+//	{
+//		a[i] = i;
+//	}
+//	for (int i = 0; i < 5; i++)
+//	{
+//		printf("a[%d] = %d\n", i, a[i]);
+//	}
+//	return 0;
+//}
+//新明解C言語入門演習5-2
+//int main()
+//{
+//	int a[5];
+//	for (int i = 0; i < 5; i++)
+//	{ 
+//		a[i] = 5 - i;
+//	}
+//	for (int i = 0; i < 5; i++)
+//	{
+//		printf("a[%d] = %d\n", i, a[i]);
+//	}
+//
+//	return 0;
+//}
+//新明解C言語入門演習5-3
+//int main()
+//{
+//	int a[5] = { 5,4,3,2,1 };
+//	for (int i = 0; i < 5; i++)
+//	{
+//		printf("a[%d] = %d\n", i, a[i]);
+//	}
+//	return 0;
+//}
+//新明解C言語入門演習5-4
+//#define NUMBER 7
+//int main()
+//{
+//	int x[NUMBER];
+//	for (int i = 0; i < NUMBER; i++)
+//	{
+//		printf("x[%d] :", i);
+//		scanf("%d", &x[i]);
+//	}
+//	for (int i = 0; i < (NUMBER / 2); i++)
+//	{
+//		int t = x[i];
+//		x[i] = x[NUMBER - 1 - i];
+//		x[NUMBER - 1 - i] = t;
+//	}
+//	puts("反転しました。");
+//	for (int i = 0; i < NUMBER; i++)
+//	{
+//		printf("x[%d] = %d\n", i, x[i]);
+//	}
+//	return 0;
+//}
+//新明解C言語入門演習5-5
+//int main()
+//{
+//	double a;
+//	int b;
+//	a = b = 1.5;//bは1になり、aは1.00になる
+//	printf("a = %f\n", a);
+//	printf("b = %d\n", b);
+//	return 0;
+//}
+//新明解C言語入門演習5-6
+//#define NUMBER 100
+//int main()
+//{
+//	int num;
+//	printf("データ数:");
+//	scanf("%d", &num);
+//	int x[NUMBER];
+//	for (int i = 0; i < num; i++)
+//	{
+//		printf("%d番 :", i + 1);
+//		scanf("%d", &x[i]);
+//	}
+//	for (int i = 0; i < num; i++)
+//	{
+//		if (i == 0)
+//			printf("{%d, ", x[i]);
+//		else if (i > 0 && i < num - 1)
+//			printf("%d, ", x[i]);
+//		else
+//			printf("%d}", x[i]);
+//	}
+//	return 0;
+//}
+//新明解C言語入門演習5-7
+//#define NUMBER 120
+//int main()
+//{
+//	int num;
+//	int tensu[NUMBER];
+//	int bunpu[11] = { 0 };
+//	printf("人数を入力せよ:");
+//	do
+//	{
+//		scanf("%d", &num);
+//		if (num<1 || num>NUMBER)
+//			printf("\a1～%dで入力せよ:", NUMBER);
+//	} while (num<1 || num>NUMBER);
+//	printf("%d人の点数を入力せよ。\n", num);
+//	for (int i = 0; i < num; i++)
+//	{
+//		printf("%2d番:", i + 1);
+//		do
+//		{
+//			scanf("%d", &tensu[i]);
+//			if (tensu[i] < 0 || tensu[i]>100)
+//				printf("\a0～100で入力せよ:");
+//		} while (tensu[i] < 0 || tensu[i]>100);
+//		bunpu[tensu[i] / 10]++;
+//	}
+//	puts("\n---分布グラフ---");
+//	for (int i = 0; i < 10; i++)
+//	{
+//		printf("%3d～%3d : ", i * 10, i * 10 + 9);
+//		for (int j = 0; j < bunpu[i]; j++)
+//			putchar('*');
+//		putchar('\n');
+//	}
+//	printf("     100 : ");
+//	for (int j = 0; j < bunpu[10]; j++)
+//		putchar('*');
+//	putchar('\n');
+//	return 0;
+//}
+//新明解C言語入門演習5-8
+//#define NUMBER 120
+//int main()
+//{
+//	int num;
+//	int i, j;
+//	int tensu[NUMBER];
+//	int bunpu[11] = { 0 };
+//	printf("人数を入力せよ:");
+//	do
+//	{
+//	scanf("%d", &num);
+//	if (num<1 || num>NUMBER)
+//			printf("\a1～%dで入力せよ:", NUMBER);
+//	} while (num<1 || num>NUMBER);
+//	printf("%d人の点数を入力せよ。\n", num);
+//	for (i = 0; i < num; i++)
+//	{
+//		printf("%2d番:", i + 1);
+//		do
+//		{
+//			scanf("%d", &tensu[i]);
+//			if (tensu[i] < 0 || tensu[i]>100)
+//				printf("\a0～100で入力せよ:");
+//		} while (tensu[i] < 0 || tensu[i]>100);
+//		bunpu[tensu[i] / 10]++;
+//	}
+//	puts("\n---分布グラフ---");
+//	int max = bunpu[0];
+//	for (i = 1; i <= 10; i++)//分布の数を順番に比較し,一番多い分布を探し出す
+//	{
+//		if (max < bunpu[i])
+//		{
+//			max = bunpu[i];
+//		}
+//	}
+//	for (i = max; i > 0; i--)//つまり,一番多い分布の数が行数になる
+//	{
+//		for (j = 0; j <= 10; j++)
+//		{
+//			if (i == bunpu[j])//ｊ列の分布にあるか、ないかの判断
+//			{
+//				printf(" * ");
+//				bunpu[j]--;//一個表示したので,分布の数を１減らす
+//			}
+//			else
+//			{
+//				printf("   ");
+//			}
+//		}
+//		putchar('\n');
+//	}
+//	printf("---------------------------------\n");
+//	printf(" 0 10 20 30 40 50 60 70 80 90 100\n");
+//	//for (i = 0; i <= 33; i++)
+//	//{
+//	//	putchar('-');
+//	//}
+//	//putchar('\n');
+//	//for (i = 0; i <= 10; i++)
+//	//{
+//	//	if (i == 10)
+//	//	{
+//	//		puts(" 100");
+//	//	}
+//	//	else
+//	//		printf("%3d", i * 10);
+//	//}
+//	return 0;
+//}
+//新明解C言語入門演習5-9
+//int main()
+//{
+//	int a[5];
+//	int b[5];
+//	int i;
+//	for (i = 0; i < 5; i++)
+//	{
+//		printf("a[%d] : ", i);
+//		scanf("%d", &a[i]);
+//	}
+//	for (i = 0; i < 5; i++)
+//	{
+//		b[i] = a[5 - i - 1];
+//	}
+//	puts("  a    b");
+//	puts("---------");
+//	for (i = 0; i < 5; i++)
+//	{
+//		printf("%4d%4d\n", a[i], b[i]);
+//	}
+//	return 0;
+//}
+//新明解C言語入門演習5-10
+//4行3列と3行4列の積
+//int main()
+//{
+//	int i, j, k;
+//  int sum = 0;
+//	int a[4][3];
+//	int b[3][4];
+//	printf("4行3列の数を入力せよ:\n");
+//	for (i = 0; i < 4; i++)
+//	{
+//		//for (j = 0; j < 3; j++)
+//		//{
+//		//	printf("%d行%d列の数は:", i + 1, j + 1);
+//		//	scanf("%d", &a[i][j]);
+//		//}
+//		scanf("%d %d %d", &a[i][0], &a[i][1], &a[i][2]);
+//	}
+//	printf("3行4列の数を入力せよ:\n");
+//	for (i = 0; i < 3; i++)
+//	{
+//		//for (j = 0; j < 4; j++)
+//		//{
+//		//	printf("%d行%d列の数は:", i + 1, j + 1);
+//		//	scanf("%d", &a[i][j]);
+//		//}
+//		scanf("%d %d %d %d", &b[i][0], &b[i][1], &b[i][2], &b[i][3]);
+//	}
+//	printf("4行3列と3行4列の積は:\n");
+// //行列の積の公式は:
+// //m行n列型　*　n行p列型　=　m行p列型
+//	for (k = 0; k < 4; k++)
+//	{
+//		for (i = 0; i < 4; i++)
+//		{
+//			for (j = 0; j < 3; j++)
+//			{
+//				sum = sum + (a[k][j] * b[j][i]);
+//			}
+//			printf("%3d", sum);
+//			sum = 0;
+//		}
+//		putchar('\n');
+//	}
+//	return 0;
+//}
+//新明解C言語入門演習5-11
+//int main()
+//{
+//	int i, j ;
+//	int tensu[6][2];
+//	int sum1 = 0, sum2 = 0, sum3 = 0;
+//	for (i = 0; i < 6; i++)
+//	{
+//		printf("%d人目の成績は: \n", i + 1);
+//		printf("国語 :"); scanf("%d", &tensu[i][0]);
+//		printf("数学 :"); scanf("%d", &tensu[i][1]);
+//	}
+//	for (i = 0; i < 6; i++)
+//	{
+//		sum1 += tensu[i][0];
+//		sum2 += tensu[i][1];
+//	}
+//	printf("国語の合計点は :%3d 平均点は :%.2f\n", sum1, (double)sum1 / 6);
+//	printf("数学の合計点は :%3d 平均点は :%.2f\n", sum2, (double)sum2 / 6);
+//	for (i = 0; i < 6; i++)
+//	{
+//		for (j = 0; j < 2; j++)
+//		{
+//			sum3 += tensu[i][j];
+//		}
+//		printf("%d人目の合計点は : %d 平均点は :%.2f\n", i + 1, sum3, (double)sum3 / 2);
+//		sum3 = 0;
+//	}
+//	return 0;
+//}
+//新明解C言語入門演習5-12
+//int main()
+//{
+//	int tensu[2][4][3] = { {{91,63,78},{67,72,46},{89,34,53},{32,54,34}},{{97,67,82},{73,43,46},{97,56,21},{85,46,35}} };
+//	//多次元配列の場合:　配列の名前[回数][行数][列数]
+//	//tensu[2][4][3]: 2回　4人　3科目の成績
+//	int sum[4][3];
+//	int i, j, k;
+//	for (i = 0; i < 4; i++)
+//	{
+//		for (j = 0; j < 3; j++)
+//		{
+//			sum[i][j] = tensu[0][i][j] + tensu[1][i][j];
+//		}
+//	}
+//	for (k = 0; k < 2; k++)
+//	{
+//		printf("%d回目の点数\n", k + 1);
+//		for (i = 0; i < 4; i++)
+//		{
+//			for (j = 0; j < 3; j++)
+//			{
+//				printf("%4d", tensu[k][i][j]);
+//			}
+//			putchar('\n');
+//		}
+//	}
+//	puts("合計点");
+//	for (i = 0; i < 4; i++)
+//	{
+//		for (j = 0; j < 3; j++)
+//		{
+//			printf("%4d", sum[i][j]);
+//		}
+//		putchar('\n');
+//	}
+//	return 0;
+//}
