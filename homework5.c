82774ade83cef91187b0894250b84e9a4de03e19
strangestQ <jimmyoliven@yahoo.co.jp>
2021-11-27 18:25:05 +0900

    練習

---
+++ homework5.c
@@ -0,0 +1,101 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//V–¾‰ğCŒ¾Œê“ü–å‰‰K9-1
+//int main()
+//{
+//	char str[] = "ABC\0DEF";
+//	printf("•¶š—ñstr‚Í\"%s\"‚Å‚·B\n", str);
+//	return 0;
+//}
+//V–¾‰ğCŒ¾Œê“ü–å‰‰K9-2
+//int main()
+//{
+//	char s[] = "ABC";
+//	printf("\"%s\"\n", s);
+//	s[0] = '\0';
+//	printf("\"%s\"\n", s);
+//	return 0;
+//}
+//V–¾‰ğCŒ¾Œê“ü–å‰‰K9-3
+//#define NUM 5
+//
+//int main()
+//{
+//	char s[NUM][128];
+//	int i = 0, j = 0;
+//	char stop[] = "$$$$$";
+//	for (i = 0; i < NUM; i++)
+//	{
+//		printf("s[%d] : ", i);
+//		scanf("%s", s[i]);
+//		if (strcmp(s[i], stop) == 0)
+//			break;
+//	}
+//	for (i = 0; i < NUM; i++)
+//	{
+//		if (strcmp(s[i], stop) == 0)
+//			break;
+//		printf("s[%d] = \"%s\"\n", i, s[i]);
+//	}
+//	return 0;
+//}
+//V–¾‰ğCŒ¾Œê“ü–å‰‰K9-4
+//void null_string(char s[])
+//{
+//	s[0] = '\0';
+//}
+//int main()
+//{
+//	char s[] = "ABCD";
+//	printf("\"%s\"\n", s);
+//	null_string(s);
+//	printf("\"%s\"\n", s);
+//	return 0;
+//}
+//V–¾‰ğCŒ¾Œê“ü–å‰‰K9-5
+//int str_char(const char s[], int c)
+//{
+//	int i = 0;
+//	for (i = 0; s[i] != '\0'; i++)
+//	{
+//		if (s[i] == c)
+//			return i;
+//	}
+//		return -1;
+//}
+//int main()
+//{
+//	char s[10];
+//	scanf("%s", s);
+//	int tmp = str_char(s, 'c');
+//	if (tmp == -1)
+//		printf("•¶šc‚Í•¶š—ñs‚ÉŠÜ‚Ü‚ê‚Ä‚¢‚Ü‚¹‚ñB\n");
+//	else
+//		printf("•¶šc‚Í•¶š—ñs‚Ì%d”Ô–Ú‚É‚ ‚è‚Ü‚·B“Yš‚Í%d‚Å‚·B\n", tmp+1,tmp);
+//	return 0;
+//}
+//V–¾‰ğCŒ¾Œê“ü–å‰‰K9-6
+
+//V–¾‰ğCŒ¾Œê“ü–å‰‰K9-7
+
+//V–¾‰ğCŒ¾Œê“ü–å‰‰K9-8
+
+//V–¾‰ğCŒ¾Œê“ü–å‰‰K9-9
+
+//V–¾‰ğCŒ¾Œê“ü–å‰‰K9-10
+
+//V–¾‰ğCŒ¾Œê“ü–å‰‰K9-11
+
+//V–¾‰ğCŒ¾Œê“ü–å‰‰K9-12
+
+//V–¾‰ğCŒ¾Œê“ü–å‰‰K10-1
+
+//V–¾‰ğCŒ¾Œê“ü–å‰‰K10-2
+
+//V–¾‰ğCŒ¾Œê“ü–å‰‰K10-3
+
+//V–¾‰ğCŒ¾Œê“ü–å‰‰K10-4
+
+//V–¾‰ğCŒ¾Œê“ü–å‰‰K10-5
